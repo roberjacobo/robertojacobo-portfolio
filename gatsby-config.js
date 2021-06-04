cb4e7967029f98f3c338615024a7c202278b18da
Rober <robjacobox@gmail.com>
2021-06-03 22:27:24 -0500

    portfolio new structure

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
 module.exports = {
-  siteMetadata: {
-    title: "Roberto Jacobo Portfolio"
-  },
-  plugins: [`gatsby-plugin-gatsby-cloud`]
-};
+  /* Your site config here */
+  plugins: [`gatsby-plugin-gatsby-cloud`],
+}
