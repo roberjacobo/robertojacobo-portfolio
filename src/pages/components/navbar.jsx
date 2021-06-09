7809662e85c2f6a15d00f6f22272b1af2e486ead
Rober <robjacobox@gmail.com>
2021-06-08 22:54:51 -0500

    modal cv without animation

--- src/pages/components/navbar.jsx
+++ src/pages/components/navbar.jsx
@@ -6,11 +6,11 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <div className="list-container">
-        <button>About Me</button>
-        <button>What I Do</button>
-        <button>Resume</button>
-        <button>Portfolio</button>
-        <button>Contact</button>
+        <button className="btn-navbar">About Me</button>
+        <button className="btn-navbar">What I Do</button>
+        <button className="btn-navbar">Resume</button>
+        <button className="btn-navbar">Portfolio</button>
+        <button className="btn-navbar">Contact</button>
       </div>
     </div>
   );
